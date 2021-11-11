423c475726da9b498d185eb2da68cf789aa39cd5
keiyanishio <keiyan@al.insper.edu.br>
2021-11-11 17:30:59 -0300

    Começando

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import cryptoRandomString from 'crypto-random-string';
+import axios from "axios";
+
 export default function Home(){
-    const code_verifier = cryptoRandomString({length: 128, type: 'url-safe'});
-    const url = "https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=046e703006cab4f534ee6af14f564962&code_challenge="+code_verifier+"&state=RequestID42&redirect_uri=http://localhost:3000/token";
-    return <a href={url}> Teste </a>;
+  const [animes, set_animes] = useState([{}]);
+  useEffect(() => {
+    const headers={'Accept': 'Applicaton/json'}
+    axios.get('https://api.jikan.moe/v3/season/2021/fall', {headers:headers})
+    .then((response)=>
+      set_animes(response.data.anime))
+  }, []);
+
+
+  function addAnime(title, img, mal_id){
+    const headers={'Accept': 'application/json', 'Content-Type': 'application/json'}
+    const url = 'http://127.0.0.1:8000/api/animes/'
+    const data = {'title':title, 'img':img, 'mal_id': mal_id}
+    axios.post(url,data, {headers:headers})
+
   }
+
+  return (
+    <div>
+    {animes.map((anime) => (
+      <div> 
+      <h1>{anime.title}</h1>
+      <button onClick={addAnime(anime.title, anime.image_url, anime.id_mal)}>Default</button>;
+      <img src = {anime.image_url}/>
+      </div>))}
+    </div>
+  )
+
+}
