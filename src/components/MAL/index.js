0fd9d02192d509a6c455f599ae512f074b56355f
keiyanishio <keiyan@al.insper.edu.br>
2021-11-13 19:12:50 -0300

    my name list

--- src/components/MAL/index.js
+++ src/components/MAL/index.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 
-export default function MAL(){s
-    return <h1>Deez</h1>;
+export default function MAL(){
+    const [animes, set_animes] = useState([{}]);
+    function loadData(){
+        axios.get('http://127.0.0.1:8000/api/animes/')
+        .then((response)=>
+          set_animes(response.data))
+    }
+    
+    useEffect(() => {
+    loadData();
+    }, []);
+
+    return (
+        <div>
+        {animes.map((anime) => (
+          <div key={anime.mal_id}> 
+            <h1>{anime.title}</h1>
+            <img src = {anime.img} alt={`poster do ${anime.title}`}/>
+          </div>
+          ))}
+        </div>
+      )
+    
 };
